07e4eb268a5c82fba61ab73447fbbce03f77a22a
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-10 13:47:01 +0530

    sort colors

---
+++ Sorting/sortcolors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0;
+        int one=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(zero>0){
+                nums[i]=0;
+                zero--;
+            }
+            else if(one>0){
+                nums[i]=1;
+                one--;
+            }
+            else{
+                nums[i]=2;
+            }
+        }
+    }
+};
